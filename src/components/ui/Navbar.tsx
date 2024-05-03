b5885e69d5c5cfed7234070960e987b0eb5eabb7
Alessandro-Velasco <velasco.alessandro@gmial.com>
2024-05-03 16:07:22 +0200

    Goggins-ai-coach app create project and compnents

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
 import React from 'react'
+
+const routes = [
+    {
+        name: "Chat",
+        path: "/",
+    },
+    {
+        name: "Profile",
+        path: "/profile",
+    },
+];
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='p-4 flex flex-row justify-between items-center bg-black text-white'>
+        <Link href="/">
+            <h1 className='text-2xl font-bold'>Goggins AI</h1>
+        </Link>
+        <div>
+            
+            <Link href="/about">
+                <a className="mr-4">About</a>
+            </Link>
+        </div>
+
+    </div>
   )
 }
 
