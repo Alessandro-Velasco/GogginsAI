da049b44f9f7e076c892a62ea72555735eca26de
Alessandro-Velasco <velasco.alessandro@gmial.com>
2024-05-04 14:11:12 +0200

    create chatpage and profile page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/ui/Navbar";
 
 
+
 export default function AppLayout({
  children,
   }: Readonly<{
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const routes = [
@@ -13,20 +17,30 @@ const routes = [
 ];
 
 function Navbar() {
+    const pathname = usePathname();
+
   return (
     <div className='p-4 flex flex-row justify-between items-center bg-black text-white'>
         <Link href="/">
             <h1 className='text-2xl font-bold'>Goggins AI</h1>
         </Link>
-        <div>
-            
-            <Link href="/about">
-                <a className="mr-4">About</a>
-            </Link>
-        </div>
+        <div className='flex gap-x-6 text-lg items-center'>
+            {routes.map((route, idx) => (
+              <Link
+                key={idx}
+                href={route.path}
+                className={
+                    pathname === route.path ? "border-b-2 border-yellow-500" : ""
+                }
+               >
+                {route.name}
+              </Link>
+        ))}
 
-    </div>
-  )
-}
+         <UserButton afterSignOutUrl="/" />
+        </div>
+      </div> 
+    );
+}    
 
 export default Navbar;
