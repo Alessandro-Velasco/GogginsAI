db588fd806caab2a0a4ef09ada228b7e12b169ca
Alessandro Velasco <velasco.alessandro@gmail.com>
2024-07-09 21:50:58 +0200

    change hfull on page tsx

--- goggins-ai-coach/src/app/(app)/page.tsx
+++ goggins-ai-coach/src/app/(app)/page.tsx
@@ -4,9 +4,13 @@ import React, { useState } from 'react';
 function ChatPage() {
   const [fetching, setFetching] = useState(true);
   return (
-    <div className="w-screen h-screen flex flex-col bg-black text-white">ChatPage
+    <div className="w-screen h-full flex flex-col bg-black text-white">ChatPage
+    <div className='flex-grow overflow-hidden p-8 space-y-2'>
+     {fetching && <div className="text-center font-bold">Fetching...</div>}
     
-    {fetching && <div className="text-center font-bold">Fetching...</div>}
+    </div>
+    
+    
     
     
     </div>
