9e18e5b2f19d709e2b86c9e72b1f1ecdc62195ee
Alessandro Velasco <velasco.alessandro@gmail.com>
2024-07-09 21:23:20 +0200

    start working one pagetsx chat page

--- goggins-ai-coach/src/app/(app)/page.tsx
+++ goggins-ai-coach/src/app/(app)/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ChatPage() {
+  const [fetching, setFetching] = useState(false);
   return (
-    <div className="w-screen">ChatPage</div>
+    <div className="w-screen h-full flex flex-col bg-black text-white">ChatPage</div>
   )
 }
 
