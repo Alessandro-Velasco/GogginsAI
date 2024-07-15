507e9ae1e7e975b1ced174929fae209c49c967a8
Alessandro Velasco <velasco.alessandro@gmail.com>
2024-07-15 13:29:58 +0200

    update layout.tsx pending fix errors

--- goggins-ai-coach/src/app/(app)/layout.tsx
+++ goggins-ai-coach/src/app/(app)/layout.tsx
@@ -1,8 +1,36 @@
 import Navbar from "@/components/ui/Navbar";
+import { UserThread } from "@prisma/client";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+
+  const  [userThread, setUserThread] = useState<UserThread | null>(null);
+  useEffect(() => 
+    {
+  async function getUserThread() {
+    const response = await axios.get<{
+      success: boolean;
+      message?: string;
+      userThread: UserThread;
+    }>("/api/user-thread")
+  }
+
+  if (!Response.data.success || !Response.data.userThread) {
+    console.error(Response.data.message ?? "Unknow error.");
+    setUserThread(null)
+    return;
+  }
+
+  setUserThread(Response.data.userThread);
+
+  getUserThread();
+  
+   
+  }, []);
+  
     return (
    <div className="flex flex-col w-full h-full">
     <Navbar />
--- goggins-ai-coach/src/app/(app)/page.tsx
+++ goggins-ai-coach/src/app/(app)/page.tsx
@@ -1,12 +1,25 @@
-"use client"
+"use client";
+
+
+
+import { Message } from 'openai/resources/beta/threads/messages.mjs';
 import React, { useState } from 'react';
 
 function ChatPage() {
   const [fetching, setFetching] = useState(true);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const fetchMessages = async () => {
+    
+  }
   return (
     <div className="w-screen h-full flex flex-col bg-black text-white">ChatPage
     <div className='flex-grow overflow-hidden p-8 space-y-2'>
      {fetching && <div className="text-center font-bold">Fetching...</div>}
+     {messages.length === 0 && !fetching &&(
+      <div className="text-center font-bold">No messages...</div>
+     )}
+
     
     </div>
     
@@ -14,7 +27,7 @@ function ChatPage() {
     
     
     </div>
-  )
+  );
 }
 
 export default ChatPage;
